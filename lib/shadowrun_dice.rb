01a50168bb2202340a6377a05c687025e4ddfa30
unknown <rhowe@rhowe-p.musicreports.com>
2014-07-07 07:02:27 -0700

    Edge dice added to shadowrun roller.

--- lib/shadowrun_dice.rb
+++ lib/shadowrun_dice.rb
@@ -1,30 +1,45 @@
 require 'dice_roll'
 
 class ShadowrunDice
-  def initialize(listener, pool)
+  def initialize(listener, roll_description)
     @listener = listener
-    @pool = pool
+    @pool, @edge = roll_description.values_at :pool, :edge?
   end
   
   def execute
+    @all_rolls = []
     DiceRoll.new(self, {sides: 6, rolls: @pool}).execute
   end
 
   def show_roll_result(results)
-    rolls = results[:rolls]
-    hits = calc_hits(rolls)
-    glitch = was_glitch?(rolls)
-    hit_results = {
-      rolls: rolls,
-      hits: hits,
-      glitch?: glitch,
-      critical_glitch?: glitch && hits <= 0
-    }
-    @listener.show_roll_result(hit_results)
+    @all_rolls += results[:rolls]
+    show_results unless rolled_edge? results[:rolls]
   end
 
   
   private
+
+  def rolled_edge?(rolls)
+    sixes = rolls.count 6
+    if @edge && sixes > 0
+      DiceRoll.new(self, {sides: 6, rolls: sixes}).execute
+      return true
+    else
+      return false
+    end
+  end
+
+  def show_results
+    hits = calc_hits(@all_rolls)
+    glitch = was_glitch?(@all_rolls)
+    hit_results = {
+      rolls: @all_rolls,
+      hits: hits,
+      glitch?: glitch,
+      critical_glitch?: glitch && hits <= 0
+    }
+    @listener.show_roll_result hit_results
+  end
 
   def calc_hits(rolls)
     (rolls.select { |r| r >= 5 }).length
--- spec/shadowrun_dice_spec.rb
+++ spec/shadowrun_dice_spec.rb
@@ -10,37 +10,45 @@ RSpec.describe ShadowrunDice do
   end
 
   it 'should roll a number of d6 and report the results' do
-    ShadowrunDice.new(@listener, 5).execute
+    ShadowrunDice.new(@listener, {pool: 5}).execute
 
     expect(@listener.last_roll_result[:rolls]).to eq([1, 1, 1, 1, 1])
   end
 
   it 'shows the number of hits instead of the total of the rolls' do
     Entropy::RANDOMIZER.return_chain = [0, 2, 4, 1, 5]
-    ShadowrunDice.new(@listener, 5).execute
+    ShadowrunDice.new(@listener, {pool: 5}).execute
     expect(@listener.last_roll_result[:hits]).to eq(2)
   end
 
   it 'reports a glitch if at least half the rolls are 1' do
     Entropy::RANDOMIZER.return_chain = [0, 0, 0, 4, 2, 3]
-    ShadowrunDice.new(@listener, 6).execute
+    ShadowrunDice.new(@listener, {pool: 6}).execute
     expect(@listener.last_roll_result[:glitch?]).to eq(true)
   end
 
   it 'is a critical glitch if you get a glitch with no hits' do
     Entropy::RANDOMIZER.return_chain = [0, 0, 0, 2, 2, 1]
-    ShadowrunDice.new(@listener, 6).execute
+    ShadowrunDice.new(@listener, {pool: 6}).execute
     expect(@listener.last_roll_result[:critical_glitch?]).to eq(true)
   end
 
   # roll number should be integral
   it 'should round down the dice pool if not given an integral' do
-    ShadowrunDice.new(@listener, 3.7).execute
+    ShadowrunDice.new(@listener, {pool: 3.7}).execute
     expect(@listener.last_roll_result[:rolls].length).to eq(3)
   end
   
   # edge
-  
+  it 'rerolls all max rolls on an edge roll' do
+    Entropy::RANDOMIZER.return_chain = [1, 5, 5, 1, 2, 3, 2, 1]
+    ShadowrunDice.new(@listener, {pool: 6, edge?: true}).execute
+    expect(@listener.last_roll_result[:rolls].length).to eq(8)
+  end
 
-  # rule of six
+  it 'does not reroll sixes on a non-edge roll' do
+    Entropy::RANDOMIZER.return_chain = [1, 5, 5, 1, 2, 3, 2, 1]
+    ShadowrunDice.new(@listener, {pool: 6, edge?: false}).execute
+    expect(@listener.last_roll_result[:rolls].length).to eq(6)
+  end
 end
